9dfbf5ca444d1453b2cd9c3f78b50a0a7d0427a4
Om Negi <149765822+omnegi@users.noreply.github.com>
2025-07-09 16:42:19 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ from langchain_community.vectorstores import FAISS
 
 app = FastAPI()
 
-os.environ["GOOGLE_API_KEY"]="AIzaSyAqPijGdgrUTYCfBdEqbEWT__8JX1EgHvw"
+os.environ["GOOGLE_API_KEY"]="AIzaSyAro9f9awYIv3xj2YtxgQMht9tKJKSSeXs"
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  
